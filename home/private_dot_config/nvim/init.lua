e8b6fb48ca01d5dd6e5326131c3e7858b2410cc7
zhaoshenglong <zhaoshenglong@users.noreply.github.com>
2026-08-09 00:56:33 +0800

    Revert "Rewrite nvim config on LazyVim (work in progress)"
    This reverts 131b7d51fca4912e58026292c962d04059a1a47c.
    
    That commit was an incomplete automated rewrite: it deleted the
    workable customized LazyVim-based config (~8000 lines across plugins/,
    util/, lang/ etc.) and left a stripped WIP skeleton. Restore the last
    known-good config exactly as it was at c9cdd93 ("works now, but need
    to be further optimized"), mapped onto the current chezmoi layout
    (home/private_dot_config/nvim). The only commit touching nvim after
    131b7d5 was the pure path rename in 7e81ac7, so this is an exact
    restore.

--- home/private_dot_config/nvim/init.lua
+++ home/private_dot_config/nvim/init.lua
@@ -1,2 +1,56 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- 1. Initialize the configuration
+require("config").setup({
+	colorscheme = "catppuccin-mocha",
+})
+
+-- 2. Import plugins
+require("lazy").setup({
+	spec = {
+		{ import = "plugins" },
+	},
+	install = {
+		colorscheme = { "catppuccin-mocha", "habamax" },
+	},
+	checker = {
+		enabled = true, -- check for plugin updates regularly
+		notify = true, -- notify on updates
+	},
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+})
+
+-- 3. Explicityly load the colorscheme
+LazyUtil.try(function()
+	vim.cmd.colorscheme("catppuccin-mocha")
+end, {
+	msg = "Could not load your catppuccin-mocha",
+	on_error = function(msg)
+		LoongVim.error(msg)
+		vim.cmd.colorscheme("habamax")
+	end,
+})
